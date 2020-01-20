afb37d2bd183e6aa6c88109eebc09286853e3477
WPGuru4u <wpguru4u@gmail.com>
2020-01-20 14:00:15 -0500

    formatting

--- functions.php
+++ functions.php
@@ -1,21 +1,23 @@
 <?php
+
+
 //Setup Default Groups Avatar
 function my_default_get_group_avatar($avatar) {
-global $bp, $groups_template;
+	global $bp, $groups_template;
 
-if( strpos($avatar,'group-avatars') ) {
-return $avatar;
-}
-else {
-$custom_avatar = 'https://app.wheninroamtravelapp.com/wp-content/uploads/2017/06/Apple1024-1.png';
+	if ( strpos($avatar,'group-avatars') ) {
+		return $avatar;
+	} else {
+		$custom_avatar = 'https://app.wheninroamtravelapp.com/wp-content/uploads/2017/06/Apple1024-1.png';
 
-if($bp->current_action == "")
-return '<img class="avatar" alt="' . esc_attr( $groups_template->group->name ) . '" src="'.$custom_avatar.'" width="'.BP_AVATAR_THUMB_WIDTH.'" height="'.BP_AVATAR_THUMB_HEIGHT.'" />';
-else
-return '<img class="avatar" alt="' . esc_attr( $groups_template->group->name ) . '" src="'.$custom_avatar.'" width="'.BP_AVATAR_FULL_WIDTH.'" height="'.BP_AVATAR_FULL_HEIGHT.'" />';
-}
+		if ( $bp->current_action == "" )
+			return '<img class="avatar" alt="' . esc_attr( $groups_template->group->name ) . '" src="'.$custom_avatar.'" width="'.BP_AVATAR_THUMB_WIDTH.'" height="'.BP_AVATAR_THUMB_HEIGHT.'" />';
+		else
+			return '<img class="avatar" alt="' . esc_attr( $groups_template->group->name ) . '" src="'.$custom_avatar.'" width="'.BP_AVATAR_FULL_WIDTH.'" height="'.BP_AVATAR_FULL_HEIGHT.'" />';
+	}
 }
 add_filter( 'bp_get_group_avatar', 'my_default_get_group_avatar');
+
 
 function replace_class( $items, $menu, $args ) {
     // Iterate over the items to search and destroy
@@ -38,75 +40,49 @@ function replace_class( $items, $menu, $args ) {
 	}
     return $items;
 }
-
 add_filter( 'wp_get_nav_menu_items', 'replace_class', null, 3 );
+
 
 //add_action('wp_head', 'show_template');
 function show_template() {
 	global $template; print_r($template);
 }
 
-
 function ch_scripts() {
 
-  wp_localize_script( "add_bp_group", 'bp_group_add',
-        array(
-            'ajaxUrl' => admin_url( 'admin-ajax.php' )
-        )
-  );
+	wp_localize_script( "add_bp_group", 'bp_group_add',
+	    array(
+	        'ajaxUrl' => admin_url( 'admin-ajax.php' )
+	    )
+	);
 
+	wp_enqueue_script( "chscript", "/wp-content/themes/wir_17/js/script.js", array( 'jquery' ));
 
-   wp_enqueue_script( "chscript", "/wp-content/themes/wir_17/js/script.js", array( 'jquery' ));
-
-	 $dir = get_stylesheet_directory_uri();
-	 wp_enqueue_style('main', $dir . '/css/main.css' );
-//	 wp_enqueue_style('x', $d . '/css/buddypress.css' );
-//	 wp_enqueue_style('y', $d . '/css/twentythirteen.css' );
+	$dir = get_stylesheet_directory_uri();
+	wp_enqueue_style('main', $dir . '/css/main.css' );
 }
-
-
-
-
 add_action( 'wp_enqueue_scripts', 'ch_scripts' );
-
-
-function your_enqueue_scripts_function() {
-    //add this below what you currently have in your enqueue scripts function.
-
-}
-
-
-
 
 
 function new_list_process() {
     // do whatever you need in order to process the form.
 }
 add_action("wp_ajax_new_list_form", "new_list_process");
-
 //use this version for if you want the callback to work for users who are not logged in
 add_action("wp_ajax_nopriv_new_list_form", "new_list_process");
 
 
-
-
-
 function new_list() {
-
-
 	die();
 }
-
-
 // Setup Ajax action hook
 add_action( 'wp_ajax_new_list', 'new_list' );
-
 
 
 function example_ajax_request() {
 
     // The $_REQUEST contains all the data sent via ajax
-    if ( isset($_REQUEST) ) {
+    if ( isset( $_REQUEST ) ) {
 
         $fruit = $_REQUEST['fruit'];
 
@@ -127,12 +103,8 @@ function example_ajax_request() {
 		$newgroupid = groups_create_group($defaults);
 
 		echo $newgroupid;
-
-
     }
-
     // Always die in functions echoing ajax content
    die();
 }
-
 add_action( 'wp_ajax_example_ajax_request', 'example_ajax_request' );
